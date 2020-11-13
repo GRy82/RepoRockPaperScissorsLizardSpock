7e53ba78e5ebad0544cc2d7fc4a395231855bd18
GRy82 <gwryan@buffalo.edu>
2020-11-13 15:53:20 -0500

    RightSpock and LeftSpock functions completed

--- RPSLS/VisualSimulation.cs
+++ RPSLS/VisualSimulation.cs
@@ -17,32 +17,74 @@ namespace RPSLS
             }
             else
             { 
-                if (leftGesture.name == "Spock" && rightGesture.name == "Spock")
-                {
+                if (leftGesture.name == "Spock" && rightGesture.name == "Spock") {
                     displayLines = TwoSpock(leftGesture.symbolLeft, rightGesture.symbolRight);
                 }
-                else if (leftGesture.name != "Spock" && rightGesture.name == "Spock")
-                {
+                else if (leftGesture.name != "Spock" && rightGesture.name == "Spock") {
                     displayLines = RightSpock(leftGesture.symbolLeft, rightGesture.symbolRight);
                 }
-                else
-                {
+                else {
                     
                 }
             }
-
             Console.WriteLine("");
             for (int i = 0; i < displayLines.Count; i++) { 
                Console.WriteLine(displayLines[i]);
             }
             Console.WriteLine("");
+        }
 
+        public List<string> LeftSpock(string leftGestureSymbol, string rightGestureSymbol)
+        {
+            List<string> displayLines = new List<string> { };
+            int counter = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                displayLines.Add("");
+                while (counter < leftGestureSymbol.Length && leftGestureSymbol[counter] != '\n')
+                {
+                    displayLines[i] += leftGestureSymbol[counter];
+                }
+            }
+            displayLines[2] += "  vs.  " + rightGestureSymbol;
+
+            return displayLines;
         }
         
-        public List<string> RightSpock(leftGestureSymbol, rightGestureSymbol)
+        public List<string> RightSpock(string leftGestureSymbol, string rightGestureSymbol)
         {
+            List<string> displayLines = new List<string> { };
+            int leftSymbolLeftSpaces = 4;
+            int vsLength = 7;
+            int symbolLength = leftGestureSymbol.Length;
+            int totalSpace = symbolLength + vsLength + leftSymbolLeftSpaces;
+            //COnstruct left part of string and necessary spacing
+            for (int i = 0; i < 5; i++){
+                displayLines.Add("");
+                if (i != 2) {
+                    for (int j = 0; j < totalSpace; j++) {
+                        displayLines[i] += " ";
+                    }
+                }
+                else {
+                    displayLines[i] += "    " + leftGestureSymbol + "  vs.  ";
+                }
+            }
+            //Construct right side of strings, containing spock.
+            int counter = 0;
+            for (int i = 0; i < displayLines.Count; i++)
+            {
+                while(counter < rightGestureSymbol.Length && rightGestureSymbol[counter] != '\n')
+                {
+                    displayLines[i] += rightGestureSymbol[counter];
+                    counter++;
+                }
+                counter++;
+            }
 
+            return displayLines;          
         }
+
 
         public List<string> TwoSpock(string leftGestureSymbol, string rightGestureSymbol)
         {
@@ -54,9 +96,10 @@ namespace RPSLS
             int maxLength = 27;
             int vsSpace = 7; //The amount of space that a "  vs.  " takes up between two spocks.
             int spacesNeeded = 0;
-
+            //For each line of the spocks printed, contruct a string to be part of a returned List<string>
             for (int i = 0; i < 5; i++) {
                 displayLines.Add("");
+                //Construct left side of spock at line 'i'
                 while (counterLeft < leftGestureSymbol.Length && leftGestureSymbol[counterLeft] != '\n')
                 {
                     tempString += (leftGestureSymbol[counterLeft]);
@@ -66,13 +109,14 @@ namespace RPSLS
                 counterLeft++;
                 displayLines[i] += tempString;
                 tempString = null;
-
+                //construct right side of spock at line 'i'
                 while (counterRight < rightGestureSymbol.Length && rightGestureSymbol[counterRight] != '\n')
                 {
                     tempString += (rightGestureSymbol[counterRight]);
                     counterRight++;
                 }
                 counterRight++;
+                //IN the 3rd line, "  vs.  " is printed in between the lines of spock. otherwise add the calculated number of spaces.
                 if (i != 2) {
                     spacesNeeded = maxLength - leftLineLength - vsSpace;
                     for (int j = 0; j < spacesNeeded; j++)
@@ -83,8 +127,9 @@ namespace RPSLS
                 else {                   
                     displayLines[i] += "  vs.  ";      
                 }
+                displayLines[i] += tempString; // add right side spock string to total string in list.
+                //reset values.
                 leftLineLength = 0;
-                displayLines[i] += tempString;
                 tempString = null;
             }
 
