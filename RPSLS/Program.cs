f1c7aa7cc66a2b76be71807a1180e767e6fc4ea1
GRy82 <gwryan@buffalo.edu>
2020-11-13 10:56:32 -0500

    Players now able to enter their own names(20 chars or less, and letter chars only) with the EnterNames and ValidateNames functions.

--- RPSLS/Program.cs
+++ RPSLS/Program.cs
@@ -14,7 +14,6 @@ namespace RPSLS
             Setup setup = new Setup();
             setup.Launch();
         }
-
     }
 }
 
--- RPSLS/Setup.cs
+++ RPSLS/Setup.cs
@@ -16,6 +16,7 @@ namespace RPSLS
             while (runProgram == true)
             {
                 List<string> gameModeOptions = new List<string> { "Single Player", "Multi Player" };
+                //Select single player, multiplater, or exit program.
                 int optionSelected = MenuServer(gameModeOptions, true); //Choose single or multiplayer
                 if (optionSelected == 3)
                 {
@@ -27,9 +28,11 @@ namespace RPSLS
                 {
                     multiPlayer = true;
                 }
-                Battle battle = LoadObjects(multiPlayer, rounds);
-                battle.Run();
 
+                List<string> names = EnterNames(optionSelected);
+
+                Battle battle = LoadObjects(multiPlayer, rounds, names);
+                battle.Run();
             }
         }
 
@@ -52,6 +55,45 @@ namespace RPSLS
             return Convert.ToInt32(rounds);
         }
 
+        public static List<string> EnterNames(int numberOfPlayers)
+        {
+            //collect names of players, according  to number of human players participating.
+            List<string> names = new List<string> { };
+            for (int i = 1; i <= numberOfPlayers; i++)
+            {
+                string proposedName = null;
+                bool validName = false;
+                while (!validName)
+                {
+                    Console.WriteLine("Player" + i + ", please enter your name.");
+                    proposedName = Console.ReadLine();
+                    validName = ValidateName(proposedName);
+                }
+                names.Add(proposedName);
+            }
+
+            return names;
+        }
+
+        public static bool ValidateName(string proposedName)
+        {
+            if (proposedName.Length > 20)
+            { //Check length
+                Console.WriteLine("Your name must be 20 characters or less");
+                return false;
+            }
+            //check for unaccepted characters.
+            for (int i = 0; i < proposedName.Length; i++)
+            {
+                if (!(proposedName[i] >= 97 && proposedName[i] <= 122) && !(proposedName[i] >= 65 && proposedName[i] <= 90))
+                {
+                    Console.WriteLine("Only letter characters accepted.");
+                    return false;
+                }
+            }
+            return true;
+        }
+
         public static bool ValidateRounds(string rounds)
         {
             foreach (char character in rounds)
@@ -70,6 +112,32 @@ namespace RPSLS
             return true;
         }
 
+        public static Battle LoadObjects(bool multiPlayer, int rounds, List<string> names)
+        {
+            //Instantiate each gesture
+            Gestures rock = new Rock();
+            Gestures paper = new Paper();
+            Gestures scissors = new Scissors();
+            Gestures lizard = new Lizard();
+            Gestures spock = new Spock();
+            //store it into a list.
+            List<Gestures> gestures = new List<Gestures> { rock, paper, scissors, lizard, spock };
 
+            //Instantiate players
+            Player player1 = new Human(gestures, names[0]);
+            Player player2;
+            if (multiPlayer == true)
+            {
+                player2 = new Human(gestures, names[1]);
+            }
+            else
+            {
+                player2 = new Computer(gestures);
+            }
+            List<Player> players = new List<Player> { player1, player2 };
+
+            Battle battle = new Battle(players, gestures, rounds, multiPlayer);
+            return battle;
+        }
     }
 }
